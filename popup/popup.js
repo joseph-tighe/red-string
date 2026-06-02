b5de3c6f841d4d8847acc0b900c5a1b245c710bd
joseph-tighe <josephjtighe@gmail.com>
2026-06-01 18:45:27 -0700

    changed manifest and debugged js

--- popup/popup.js
+++ popup/popup.js
@@ -1,37 +1,36 @@
-var Url, title, articleTitle, author;
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      Url = tabs[0].url;
-});
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      title = tabs[0].title;
-});
+(async function main() {
+    var Url, title, articleTitle, author, metaAuthor, metaPublisher, metaDate, tabId;
+    currentTab = (await chrome.tabs.query({active: true, currentWindow: true}))[0];
 
-// Listen for messages from the content script
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.message) {
-        articleTitle = '"'+document.querySelector('h1').innerText+'" ';
-    } else {
-        articleTitle = "";
-    }
+    tabId = currentTab.id;
+    Url = currentTab.url;
+    title = currentTab.title;
+
+    // Listen for messages from the content script
+    chrome.scripting.executeScript( { target: { tabId }, func: () => {
+        return document.querySelector('h1').innerText;
+    }}).then(results => {
+        articleTitle = '"'+results[0].result+'"';
     });
-//get data from meta tags
-const metaAuthor = await chrome.scripting.executeScript( { target: { tabId }, func: () => {
-    return document.querySelector('meta[name="author"], meta[property="article:author"], meta[property="og:article:author"], meta[name="byl"]')?.content
-}})[0].result;
-const metaPublisher = await chrome.scripting.executeScript( { target: { tabId }, func: () => {
-    return document.querySelector('meta[name="publisher"], meta[property="article:publisher"], meta[property="og:article:publisher"]')?.content
-}})[0].result;
-const metaDate = await chrome.scripting.executeScript( { target: { tabId }, func: () => {
-    return document.querySelector('meta[name="date"], meta[property="article:published_time"], meta[property="og:article:published_time"]')?.content
-}})[0].result;
-if (metaAuthor.includes(" ")) {
-    author = [metaAuthor.split(" ")[0], metaAuthor.split(" ")[1]].join(", ");
-} else {
-    author = metaAuthor;
-}
+    //get data from meta tags
+    let results = await chrome.scripting.executeScript( { target: { tabId }, func: () => {
+        return document.querySelector('meta[name="author"], meta[property="article:author"], meta[property="og:article:author"], meta[name="byl"]')?.content
+    }})
+    metaAuthor = results[0].result;
+    results = await chrome.scripting.executeScript( { target: { tabId }, func: () => {
+        return document.querySelector('meta[name="publisher"], meta[property="article:publisher"], meta[property="og:article:publisher"]')?.content
+    }})
+    metaPublisher = results[0].result;
+    results = await chrome.scripting.executeScript( { target: { tabId }, func: () => {
+        return document.querySelector('meta[name="date"], meta[property="article:published_time"], meta[property="og:article:published_time"]')?.content
+    }})
+    metaDate = results[0].result;
+    if (metaAuthor.includes(" ")) {
+        author = [metaAuthor.split(" ")[0], metaAuthor.split(" ")[1]].join(", ");
+    } else {
+        author = metaAuthor;
+    }
 
-const MLA = `${author}. ${articleTitle}${title}, ${metaPublisher}, ${metaDate}, ${Url}`;
-
-document.getElementById("content").innerHTML = MLA;
-
-
+    const MLA = `${author}. ${articleTitle}${title}, ${metaPublisher}, ${metaDate}, ${Url}`;
+    alert(MLA);
+})();
