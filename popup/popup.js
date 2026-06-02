baadfd55727dd05a40b3c4e3b35c562e2a3132b7
joseph-tighe <josephjtighe@gmail.com>
2026-06-01 18:28:48 -0700

    worked on debugging the js

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,4 @@
-var Url, title, articleTitle;
+var Url, title, articleTitle, author;
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       Url = tabs[0].url;
 });
@@ -15,14 +15,19 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     }
     });
 //get data from meta tags
-const metaAuthor = document.querySelector('meta[name="author"], meta[property="article:author"], meta[property="og:article:author"], meta[name="byl"]')?.content;
-const metaPublisher = document.querySelector('meta[name="publisher"], meta[property="article:publisher"], meta[property="og:article:publisher"]')?.content;
-const metaDate = document.querySelector('meta[name="date"], meta[property="article:published_time"], meta[property="og:article:published_time"]')?.content;
-
+const metaAuthor = await chrome.scripting.executeScript( { target: { tabId }, func: () => {
+    return document.querySelector('meta[name="author"], meta[property="article:author"], meta[property="og:article:author"], meta[name="byl"]')?.content
+}})[0].result;
+const metaPublisher = await chrome.scripting.executeScript( { target: { tabId }, func: () => {
+    return document.querySelector('meta[name="publisher"], meta[property="article:publisher"], meta[property="og:article:publisher"]')?.content
+}})[0].result;
+const metaDate = await chrome.scripting.executeScript( { target: { tabId }, func: () => {
+    return document.querySelector('meta[name="date"], meta[property="article:published_time"], meta[property="og:article:published_time"]')?.content
+}})[0].result;
 if (metaAuthor.includes(" ")) {
-    var author = [metaAuthor.split(" ")[0], metaAuthor.split(" ")[1]].join(", ");
+    author = [metaAuthor.split(" ")[0], metaAuthor.split(" ")[1]].join(", ");
 } else {
-    var author = metaAuthor;
+    author = metaAuthor;
 }
 
 const MLA = `${author}. ${articleTitle}${title}, ${metaPublisher}, ${metaDate}, ${Url}`;
